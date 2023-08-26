4c3d6f40903d03fdf05ac5a529fbdd5e41536331
Maxim Shipilov <skam3840@gmail.com>
2023-08-26 21:59:35 +0400

    add func title, docstring

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def up_word(word):
+    """возвращает все буквы заглавными"""
     return word.upper()
 
+
+def title(string):
+    """возвращает первые буквы слов заглавными"""
+    return string.title()
